19b0c2d03b7b812025ce208032d716c339df2e97
Alan Clucas <alan@clucas.org>
2024-11-05 14:44:53 +0000

    RW user

--- rwwf.sh
+++ rwwf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ~/workflows/rw-user.yaml
+kubectl apply -f rw-user.yaml
 sleep 1
 ARGO_TOKEN="Bearer $(kubectl get secret -n argo argo-workflows-rw-user -o=jsonpath='{.data.token}' | base64 --decode)"
 echo $ARGO_TOKEN
